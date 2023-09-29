f9f51b2638a66ca16dba7b7e5ba4f01f4981aa3f
Maxime Quertier <quertiermaxime76@gmail.com>
2023-09-29 13:29:36 +0200

    api

--- src/component/Card.js
+++ src/component/Card.js
@@ -8,18 +8,25 @@ export default class PersonList extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`https://rickandmortyapi.com/api/character?page=2`)
+    axios.get(`https://rickandmortyapi.com/api/character?page=1`)
       .then(res => {
         const persons = res.data.results;
         this.setState({ persons });
+        console.log(res.data)
       })
   }
 
   render() {
     return (
-      <ul>
-        { this.state.persons.map(person => <li>{person.name}</li>)}
-      </ul>
+      <div>
+        { this.state.persons.map(person => 
+        <div>
+          <img src={person.image}/>
+          <p>{person.name}</p>
+          <p>{person.location.name}</p>
+        </div>
+        )}
+      </div>
     )
   }
 }
