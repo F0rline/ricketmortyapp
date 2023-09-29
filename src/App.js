17910f0d99b5cb1d377f54eb9ef0d52b56589237
Maxime Quertier <quertiermaxime76@gmail.com>
2023-09-29 11:53:35 +0200

    on a tué le game

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
 import logo from '../src/assets/img/logo-rick-et-morty.png';
 import './App.css';
-import Card from './component/card';
+import Card from './component/Card';
 
-
-const url = "https://rickandmortyapi.com/api/character?page=2"
-
-
-
-
-const App = () => {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    // Ici, vous devriez faire une requête à votre API pour récupérer les images
-    // et ensuite mettre à jour l'état "images" avec les données de l'API
-  }, []); // Assurez-vous de mettre les dépendances appropriées pour le useEffect
-
-  return (
-    <div className="app">
-      {images.map((image, index) => (
-        <Card key={index} image={image} />
-      ))}
-    </div>
-  );
-}
 
 function App() {
   return (
@@ -33,29 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card/>
       </header>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-useEffect(() => {
-  fetch('https://rickandmortyapi.com/api/character?page=2')
-    .then(response => response.json())
-    .then(data => setImages(data))
-    .catch(error => console.error('Erreur:', error));
-}, []);
-
-
